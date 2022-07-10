0abf2f6faa1856f9e42a3bcb2556dd542a7f6e8c
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-10 15:26:17 -0500

    Finished Responsiveness
    -Finished responsiveness in checkout page.

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -9,31 +9,41 @@ const Checkout = () => {
     const { Items } = useContext(CartContext);
     const { removeFromCart } = useContext(CartContext);
 
+    const sum = Items.reduce((accumulator, object) => {
+        return accumulator + object.Price;
+    }, 0);
+
     return (
         <div className="cart_container">
             <div className="cart_content">
-                {Items.map((item, index) => {
-                    return (
-                        <div className="cart_item" key={index}>
-                            <div className="cart_header">
-                                <div className="item_image">
-                                    <img src={item.Image} alt="Planet" />
+                <div className="wrapper">
+                    {Items.map((item, index) => {
+                        return (
+                            <div className="cart_item" key={index}>
+                                <div className="cart_header">
+                                    <div className="item_image">
+                                        <img src={item.Image} alt="Planet" />
+                                    </div>
+                                    <div className="item_description">
+                                        <h1>Name: {item.Name}</h1>
+                                        <h1>Type: {item.Type}</h1>
+                                        <h1>Temperature: {item.Temperature}°C</h1>
+                                        <h1>Price: {item.Price} <FaMoneyBill /> </h1>
+                                    </div>
                                 </div>
-                                <div className="item_description">
-                                    <h1>Name: {item.Name}</h1>
-                                    <h1>Type: {item.Type}</h1>
-                                    <h1>Temperature: {item.Temperature}°C</h1>
-                                    <h1>Price: {item.Price} <FaMoneyBill /> </h1>
+                                <div className="cart_footer">
+                                    <div className="remove">
+                                        <button className="remove_button" onClick={() => removeFromCart(item.Name)}>Remove</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="cart_footer">
-                                <div className="remove">
-                                    <button className="remove_button" onClick={() => removeFromCart(item.Name)}>Remove</button>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
+                <div className="cart_total">
+                    <h1>Total</h1>
+                    <h1>{sum}<FaMoneyBill /></h1>
+                </div>
             </div>
         </div>
     )
