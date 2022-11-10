3cb613e8634a09d568bab27a6df2e302dd3295db
Yahir Moreno <yahirmb7568@gmail.com>
2022-11-09 21:18:40 -0600

    Added an animation to the Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-scroll';
 import { IoIosArrowDown } from 'react-icons/io';
+import { motion } from 'framer-motion';
 
 const Header = () => {
     return (
         <div className="header_container">
             <div className="header_content">
-                <h1>Shop the <span>planets</span> you love, from your home in <span>Earth</span></h1>
+                <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5 }}>Shop the <span>planets</span> you love, from your home in <span>Earth</span></motion.h1>
                 <Link to="Body" spy={true} smooth={true} duration={500} className="arrow_down">
                     <IoIosArrowDown className="arrow" />
                 </Link>
