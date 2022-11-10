7f58573dca78f4b85dfc09ba588a7eeb1e529f37
Yahir Moreno <yahirmb7568@gmail.com>
2022-11-09 21:25:01 -0600

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <div className="header_container">
             <div className="header_content">
-                <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5 }}>Shop the <span>planets</span> you love, from your home in <span>Earth</span></motion.h1>
+                <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>Shop the <span>planets</span> you love, from your home in <span>Earth</span></motion.h1>
                 <Link to="Body" spy={true} smooth={true} duration={500} className="arrow_down">
                     <IoIosArrowDown className="arrow" />
                 </Link>
